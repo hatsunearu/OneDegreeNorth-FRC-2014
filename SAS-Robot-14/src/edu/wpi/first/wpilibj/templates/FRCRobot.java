db4fe093df6747b93c3ea7625263f7e72854e4c8
kartikye <kartikye.mittal@gmail.com>
2014-01-16 12:02:45 +0800

    Created Network Tables

--- SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/FRCRobot.java
+++ SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/FRCRobot.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.templates.commands.CommandBase;
-import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -32,7 +31,7 @@ public class FRCRobot extends IterativeRobot {
      */
     public void robotInit() {
         // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
+        //autonomousCommand = new ExampleCommand();
 
         // Initialize all subsystems
         CommandBase.init();
