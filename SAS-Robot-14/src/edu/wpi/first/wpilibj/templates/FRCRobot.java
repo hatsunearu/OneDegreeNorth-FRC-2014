b06b48418a5c9154cfcd11eccea60b48f646c837
kartikye <kartikye.mittal@gmail.com>
2014-01-16 11:04:54 +0800

    Project Created AGAIN FOR THE 5TH TIME

--- SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/FRCRobot.java
+++ SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/FRCRobot.java
@@ -9,9 +9,12 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.templates.commands.CommandBase;
+import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -19,40 +22,53 @@ import edu.wpi.first.wpilibj.Timer;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class SASRobot extends IterativeRobot {
-    
-    RobotDrive driveTrain = new RobotDrive(1,2);
-    Joystick leftStick = new Joystick(1);
-    Joystick rightStick = new Joystick(2);
-    
+public class FRCRobot extends IterativeRobot {
+
+    Command autonomousCommand;
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-        
-        
+        // instantiate the command used for the autonomous period
+        autonomousCommand = new ExampleCommand();
+
+        // Initialize all subsystems
+        CommandBase.init();
+    }
+
+    public void autonomousInit() {
+        // schedule the autonomous command (example)
+        autonomousCommand.start();
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-        driveTrain.tankDrive(leftStick,rightStick);
+        Scheduler.getInstance().run();
+    }
+
+    public void teleopInit() {
+	// This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to 
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        autonomousCommand.cancel();
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+        Scheduler.getInstance().run();
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+        LiveWindow.run();
     }
-    
 }
