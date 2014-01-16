1ff7610aa7cb5a79a95e0f0b7efd3132cf3a1ad5
kartikye <kartikye.mittal@gmail.com>
2014-01-16 20:52:12 +0800

    changes

--- SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
+++ SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.OI;
 import edu.wpi.first.wpilibj.templates.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.templates.subsystems.NetworkTables;
-
+import edu.wpi.first.wpilibj.templates.subsystems.Launcher;
 /**
  * The base for all commands. All atomic commands should subclass CommandBase.
  * CommandBase stores creates and stores each control system. To access a
@@ -17,6 +17,7 @@ public abstract class CommandBase extends Command {
     // Create a single static instance of all of your subsystems
     public static NetworkTables networkTables = new NetworkTables();
     public static Drivetrain drivetrain = new Drivetrain();
+    public static Launcher launcher = new Launcher();
 
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
