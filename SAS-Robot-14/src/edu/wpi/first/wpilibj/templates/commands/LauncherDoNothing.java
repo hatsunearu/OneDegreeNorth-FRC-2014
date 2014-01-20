752c2b78121a484d025b830bb8d7f8221eedd57b
kartikye <kartikye.mittal@gmail.com>
2014-01-20 12:43:44 +0800

    some changes that make the robot move

--- SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/commands/LauncherDoNothing.java
+++ SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/commands/LauncherDoNothing.java
@@ -13,7 +13,7 @@ public class LauncherDoNothing extends CommandBase {
     
     public LauncherDoNothing() {
         // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(launcher);
     }
 
     // Called just before this Command runs the first time
