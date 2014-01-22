2fa2b0147148c61f20b101c770267d3a7dc19dfa
Jayendra Minakshisundar <minakshisu32056@sas.edu.sg>
2014-01-22 11:22:44 +0800

    DriveDistance command added, getDistance updated

---
+++ SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/commands/DriveDistance.java
@@ -0,0 +1,42 @@
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.templates.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.templates.subsystems.Vision;
+
+/**
+ *
+ * @author Samsung
+ */
+public class DriveDistance extends CommandBase {
+    
+    private boolean stopped = false;
+    
+    public DriveDistance() {
+        requires(drivetrain);
+        requires(vision);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    protected void execute() {
+        drivetrain.tankDrive(0, 0);
+        stopped = true;
+        new UpdateDistance();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return stopped;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
