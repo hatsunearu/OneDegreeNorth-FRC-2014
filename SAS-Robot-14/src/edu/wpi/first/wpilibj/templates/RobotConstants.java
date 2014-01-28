60c1bbb2030cdd6deaf6309da2c442e2a31cf12a
AlexinaHaefner <cinafoil@gmail.com>
2014-01-28 10:57:34 +0800

    Implemented Robot Preferences for PI constants

--- SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/RobotConstants.java
+++ SAS-Robot-14/src/edu/wpi/first/wpilibj/templates/RobotConstants.java
@@ -4,12 +4,16 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Preferences;
+
 /**
  *
  * @author hatsunearu
  * Class containing various constants so it's easy to modify
  */
 public class RobotConstants {
+    
+    static Preferences pref;
     
     //LauncherReset
     public static final double retractTime = 0.5;
@@ -27,9 +31,9 @@ public class RobotConstants {
     public static final double defaultRetractSpeed = 0.5;
     
     //Default Drive Distance
-    public static final double DriveDistance = 3;
-    public static final double DriveDistanceKp = 0.44;
-    public static final double DriveDistanceKi = 0.00133;
-    public static final double DriveDistanceSpeedLimit = 0.6;
-    public static final double DriveDistanceTimeout = 10;
+    public static final double DriveDistance = pref.getDouble("DriveDistanceKp", 3);
+    public static final double DriveDistanceKp = pref.getDouble("DriveDistanceKp", .44);
+    public static final double DriveDistanceKi = pref.getDouble("DriveDistanceKp", 0.00133);
+    public static final double DriveDistanceSpeedLimit = pref.getDouble("DriveDistanceKp", .6);
+    public static final double DriveDistanceTimeout = pref.getDouble("DriveDistanceKp", 10);
 }
